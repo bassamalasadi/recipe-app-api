05c0b741254b981826b71996d448c7508dcf7590
bassamalasadi <bassamalasadi@gmail.com>
2021-04-02 23:07:52 +0300

    Fix the unittest for authentication users

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -69,7 +69,7 @@ class PublicUserApiTests(TestCase):
         create_user(**payload)
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertNotIn('token', res.data)
+        self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_create_invalid_credentials(self):
