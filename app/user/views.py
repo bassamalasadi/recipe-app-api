b4a5ac167863aa2f11cb7f57b35a93e8434b1f28
bassamalasadi <bassamalasadi@gmail.com>
2021-04-02 23:12:24 +0300

    Fix flake errors

--- app/user/views.py
+++ app/user/views.py
@@ -9,6 +9,7 @@ class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializers
 
+
 class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
     serializer_class = AuthTokenSerializer
